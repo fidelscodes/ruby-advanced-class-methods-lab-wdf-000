d2a1c5bb7b40608b4ea536e5432850b02daccfbd
fidelscodes <fidelscodes@gmail.com>
2016-06-30 13:11:27 -0400

    Update with solution

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,7 @@
+require "pry"
+
+# TODO: remove all comments before pushing
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +14,64 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    self.all << self.new
+    self.all.last
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+
+    # why doesn't this work?
+    # self.new.name = name
+    # self.name
+    # binding.pry
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    # in the block, song is equal to each object in @@all
+    # remember that @@all is holding all Song objects created, not just names
+    self.all.detect { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.include?(self.find_by_name(name))
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+    # binding.pry
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    song_info = filename.split(/\.mp3|\s-\s/)
+    song = self.new # self = Song / Same as calling Song.new
+    song.name = song_info[1]
+    song.artist_name = song_info[0]
+    song
+    # binding.pry
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+# binding.pry
 end
